4cd798266c71afa9f0e7db4288bb0e94a97e89bb
shu <shu-yang.chia@endclothing.com>
2017-08-22 10:49:54 +0100

    amending header

--- src/app/shared/statistics-api.service.ts
+++ src/app/shared/statistics-api.service.ts
@@ -19,6 +19,7 @@ export class StatisticsApiService {
         let headers = new Headers();
         headers.append("Content-Type", 'application/json');
         headers.append("Accept", "application/json");
+        headers.append("Access-Control-Allow-Origin", "https://calm-eyrie-82472.herokuapp.com/");
         headers.append("Access-Control-Allow-Credentials", "true");
 
         return new RequestOptions({ headers: headers, withCredentials: true });
